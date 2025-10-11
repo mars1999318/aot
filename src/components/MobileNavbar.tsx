79fb53411b4a5580ebc9ae991c681316cea6c431
lilin599831 <lilin599831@gmail.com>
2025-10-11 14:46:06 +0800

    Fix React import in MobileNavbar.tsx

--- src/components/MobileNavbar.tsx
+++ src/components/MobileNavbar.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Home, Coins, Users, Heart } from 'lucide-react'
 import { WalletConnect } from './WalletConnect'
 import { LanguageSwitcher } from './LanguageSwitcher'
